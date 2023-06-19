005def2e77f0d6816797516240bf24424cb980d9
shoxrux0202q <smuxiddinov@students.wiut.uz>
2023-06-19 09:28:50 +0500

    first commit

---
+++ homework/square.go
@@ -0,0 +1,13 @@
+package homework
+
+import (
+	"math"
+)
+
+
+func Square (radius float64) float64 {
+	area := math.Pi * math.Pow(radius, 2)
+	return area
+}
+	
+
---
+++ reversArray.go
@@ -0,0 +1,4 @@
+package homework 
+
+func ReverseArray
+
